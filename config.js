68760d700c5193a1a3448c416f7f66549c3164d0
Dmitriy Vovchinskiy <75983297+DmitryV2044@users.noreply.github.com>
2023-11-11 15:15:15 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//     API_URL: 'https://eventracker.ru/api/v1/',
+//     developmentToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NzAzNTA4MiwiZXhwIjoxNjk4MjQ0NjgyfQ.-QCeWcr7HYuc6yUyQkRpqHB0Gcs8wCyhuzZGRj8PCR0',
+//     axiosAuthConfig: {
+//         headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NzAzNTA4MiwiZXhwIjoxNjk4MjQ0NjgyfQ.-QCeWcr7HYuc6yUyQkRpqHB0Gcs8wCyhuzZGRj8PCR0`}
+//     }
+// }
